e193619fc94c64bb67b32e619c6e77174f4d8f8a
himanshu14k <hk1974@protonmail.com>
2022-11-28 02:22:14 +0530

    Nothing new

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ except Exception as e:
 if __name__ == '__main__':
     try:
         # serve(socketio.run(app, debug=True))
-        socketio.run(app, debug=True)
+        socketio.run(app, debug=True, port=5006)
     except Exception as e:
         print("Error : Error ouccured in root file.")
         print("Exception is : ", str(e))
